8e80005f756f630b058b0f5eca91dbb538cd3db4
Francesco Aracu <48188510+francescoaracu@users.noreply.github.com>
2025-05-08 23:37:25 +0200

    Update vavoo.py

--- vavoo.py
+++ vavoo.py
@@ -3,13 +3,13 @@ import json
 import re
 import os
 
-LINKMFP = "https://tuonome.hf.space"
-PASSWORD = "tuapassword"
+LINKMFP = "https://frankuaku-ealloradai.hf.space"
+PASSWORD = "PobaWave97!"
 
 PROXY = f"{LINKMFP}/proxy/hls/manifest.m3u8?api_password={PASSWORD}&d="
 HEADER = "&h_user-agent=VAVOO/2.6&h_referer=https://vavoo.to/"
-USR = "TUO_USER_GITHUB"
-BRANCH = "TUO_BRANCH_GITHUB"
+USR = "francescoaracu"
+BRANCH = "main"
 
 OUTPUT_FILE = "channels_italy.m3u8"
 BASE_URL = "https://vavoo.to"
