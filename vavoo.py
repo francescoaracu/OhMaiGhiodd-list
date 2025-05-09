54326bc3ec5ddf82059819b4e95d920f8a16b8e5
Francesco Aracu <48188510+francescoaracu@users.noreply.github.com>
2025-05-09 12:17:57 +0200

    Correzione

--- vavoo.py
+++ vavoo.py
@@ -9,7 +9,7 @@ PASSWORD = "PobaWave97!"
 PROXY = f"{LINKMFP}/proxy/hls/manifest.m3u8?api_password={PASSWORD}&d="
 HEADER = "&h_user-agent=VAVOO/2.6&h_referer=https://vavoo.to/"
 USR = "francescoaracu"
-BRANCH = "main"
+BRANCH = "OhMaiGhiodd-list"
 
 OUTPUT_FILE = "channels_italy.m3u8"
 BASE_URL = "https://vavoo.to"
